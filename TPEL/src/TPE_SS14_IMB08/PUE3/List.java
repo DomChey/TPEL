671b40dc2f3b0a04d69c8d92110d9e41bfa22937
Domi <Domi@10.10.102.63>
2014-05-12 11:39:41 +0200

    ListImpl

--- TPEL/src/TPE_SS14_IMB08/PUE3/List.java
+++ TPEL/src/TPE_SS14_IMB08/PUE3/List.java
@@ -7,45 +7,45 @@ package TPE_SS14_IMB08.PUE3;
 */
 public interface List<T> extends Iterable<T> {
 /**
-* ¨Uberpr¨uft, ob ein Element bereits vorhanden ist.
+* Überprüft, ob ein Element bereits vorhanden ist.
 *
-* @param e Element auf das gepr¨uft werden soll
+* @param e Element auf das geprüft werden soll
 * @return true wenn vorhanden, andernfalls false
 */
 public abstract boolean contains(Object e);
 /**
-* F¨ugt am Ende ein Element hinzu.
+* Fügt am Ende ein Element hinzu.
 *
-* @param e Element, das hinzugef¨ugt werden soll.
+* @param e Element, das hinzugefügt werden soll.
 * @return ist immer true
 */
 public abstract boolean add(T e);
 /**
-* F¨ugt am Anfang ein Element hinzu.
+* Fügt am Anfang ein Element hinzu.
 *
-* @param e Element, das angef¨ugt werden soll.
+* @param e Element, das angefügt werden soll.
 */
 public abstract void addFirst(T e);
 /**
-* Liefert das erste Element zur¨uck, ohne es zu entfernen.
+* Liefert das erste Element zurück, ohne es zu entfernen.
 *
 * @return das erste Element.
 */
 public abstract T peekFirst();
 /**
-* ¨Uberpr¨uft, ob Elemente vorhanden sind.
+* Überprüft, ob Elemente vorhanden sind.
 *
 * @return true wenn die Datenstruktur leer ist, andernlass false
 */
 public abstract boolean isEmpty();
 /**
-* Entfernt das erste Element und liefert es zur¨uck.
+* Entfernt das erste Element und liefert es zurück.
 *
 * @return das erste Element
 */
 public abstract T pollFirst();
 /**
-* L¨oscht den Inhalt der List.
-2*/
+* Löscht den Inhalt der List.
+*/
 public abstract void clear();
 }
---
+++ TPEL/src/TPE_SS14_IMB08/PUE3/ListImpl.java
@@ -0,0 +1,9 @@
+package TPE_SS14_IMB08.PUE3;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ListImpl<E> extends LinkedList<E> implements List<E> {
+
+
+}
