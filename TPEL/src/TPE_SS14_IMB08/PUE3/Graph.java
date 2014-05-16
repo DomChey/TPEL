98adc38a02525eed378f81119e41115d824419b7
domeix <domeix@web.de>
2014-05-16 17:26:55 +0200

    + Tiefensuche

--- TPEL/src/TPE_SS14_IMB08/PUE3/Graph.java
+++ TPEL/src/TPE_SS14_IMB08/PUE3/Graph.java
@@ -1,17 +1,89 @@
 package TPE_SS14_IMB08.PUE3;
 
-public class Graph<T> implements SearchStrategy{
+public class Graph<T> implements SearchStrategy<T>{
     private Node<T> head;
+    private NodeListImpl<T> path = new NodeListImpl<T>();
     
     public Graph(Node<T> head) {
         this.head = head;
     }
     
-    public void search(T wert, String methode){    //noch nicht durchdacht
-       head.getChildren();
+    
+    public NodeListImpl<T> copyInto(NodeListImpl<T> liste) {
+        liste.addLast(head);
+        return copyIntoRek(head, liste);
     }
     
-    public ListImpl<T> copyInto(ListImpl<T> list) {       //wie das?
-        return list;
+    private NodeListImpl<T> copyIntoRek(Node<T> anfang, NodeListImpl<T> liste) {
+        NodeListImpl<T> tmp = anfang.getChildren();
+
+        for(Node<T> knoten: tmp) {
+            if(!liste.contains(knoten)) {
+                liste.addLast(knoten);
+                liste = copyIntoRek(knoten, liste);
+            }
+        }
+        return liste;
+    }
+
+    @Override
+    public NodeListImpl<T> search(T wert) {
+//        return searchBreit(wert);
+        return searchTief(wert);
+    }
+
+    private NodeListImpl<T> searchBreit(T wert) {
+        path.clear();
+        NodeListImpl<T> matches = new NodeListImpl<T>();
+        
+        NodeListImpl<T> queue = new NodeListImpl<T>();
+        
+        path.add(head);
+        
+        //Kinder an Queue haengen
+        //Queue durchsuchen
+        //wieder Kinder anhaengen
+        //Abbruchbedingung??
+        
+        
+        
+        
+        
+        return matches;
+        
+    }
+    
+    private NodeListImpl<T> searchTief(T wert) {
+        path.clear();
+        NodeListImpl<T> matches = new NodeListImpl<T>();
+        path.addLast(head);
+        if (head.getValue().equals(wert)) {
+            matches.addLast(head);
+        }
+        return searchTiefRek(head, wert, matches);
+    }
+    
+    
+    private NodeListImpl<T> searchTiefRek(Node<T> anfang, T wert,
+                                            NodeListImpl<T> matches) {
+
+        NodeListImpl<T> tmp = anfang.getChildren();
+
+        for(Node<T> knoten: tmp) {
+            if(!path.contains(knoten)) {
+                path.addLast(knoten);
+                if (knoten.getValue().equals(wert)) {
+                    matches.addLast(knoten);
+                }
+                matches = searchTiefRek(knoten, wert, matches);
+            }
+        }
+        return matches;
+    }
+    
+    
+    @Override
+    public NodeListImpl<T> getPath() {
+        return path;
     }
 }
--- TPEL/src/TPE_SS14_IMB08/PUE3/Node.java
+++ TPEL/src/TPE_SS14_IMB08/PUE3/Node.java
@@ -28,4 +28,8 @@ public class Node<E> {
     public E getValue(){
         return this.wert;
     }
+    
+    public String toString() {
+        return getValue().toString();
+    }
 }
